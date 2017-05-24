4b5765fa013acbb758ee64a05e488ff57db7a8a4
Andrew Jones <andrewjones232@gmail.com>
2017-05-23 20:00:45 -0400

    binds script container to port 80

--- getting-started-tutorial/script.py
+++ getting-started-tutorial/script.py
@@ -1,4 +1,4 @@
 import docker
 client = docker.from_env()
-test_container = client.containers.run("cornuammonis/tutorialrepo:tag", ports = {80:4000}, detach=True)
+test_container = client.containers.run("cornuammonis/tutorialrepo:tag", ports = {80:80}, detach=True)
 print ("launched container with id " + str(test_container.id))
